2f3f5eaf8df0e1a2774645527263e2670bbdb32d
Emil <olszewski.emil13@gmail.com>
2018-03-16 15:57:54 +0100

    First Commit

---
+++ CommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Discord_BOT
+{
+    class CommandHandler
+    {
+        DiscordSocketClient _client;
+        CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommandAsync; 
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if (msg is null) return;
+            var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+            if (msg.HasStringPrefix(Config.bot.cmdPrefix, ref argPos) 
+                || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+                if(!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    Console.WriteLine(result.Error);
+            }
+        }
+    }
+}
